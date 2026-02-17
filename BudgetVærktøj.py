277258ac0e2ee7c46cb04c660e8e391193b05f49
Mohammad Jawad Bayat <Mohammad.bayatsupermand10@gmail.com>
2026-02-17 15:50:10 +0100

    Jeg har lavet udgifterne for 3 forskellige variabler. Jeg har også begyndte på en størst_udgift funktiok men ikke færdig. Vi mangler at på en måde gøre at mad variable skal være større end flere end 1

--- BudgetVærktøj.py
+++ BudgetVærktøj.py
@@ -1,13 +1,21 @@
 
 indtægter = int(input("Denne måneds indtægter: "))
-udgifter = int(input("Denne måneds udgifter: "))
+mad = int(input("Denne måneds udgifter for mad: "))
+transport = int(input("Denne måneds udgifter for transport: "))
+fornøjelser = int(input("Denne måneds udgifter for fornøjelser: "))
 
-def over_underskud(indtægter,udgifter):
-    total = indtægter - udgifter
+def over_underskud(indtægter, mad, transport, fornøjelser):
+    total = indtægter - mad - transport - fornøjelser
     if total <= 0:
         print("I denne måned er du " + str(total) + " i underskud")
     else:
         print("I denne måned har du " + str(total) + " i overskud")
     return
 
-print(over_underskud(indtægter,udgifter))
+def størst_udgift(mad,transport,fornøjelser):
+    if mad>transport:
+        print("Du har brugt flest penge på mad")
+
+
+print(over_underskud(indtægter,mad, transport, fornøjelser))
+
