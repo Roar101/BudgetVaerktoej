e8979b8672073b37ecef818585acce4abd2a83b3
Mohammad Jawad Bayat <Mohammad.bayatsupermand10@gmail.com>
2026-02-24 15:30:50 +0100

    Fixede Roars dumb sheeeit

--- BudgetVærktøj.py
+++ BudgetVærktøj.py
@@ -9,21 +9,24 @@ udgifter = {
 
 def over_underskud(indtægter, udgifter):
 
-    total = indtægter
+    return indtægter-sum(udgifter.values())
+
+
+
+def størst_udgift(udgifter):
+    højest = list(udgifter.keys())[0]
+    for kategori in udgifter:
+        if udgifter[kategori]>udgifter[højest]:
+            højest = kategori
+    return højest
+
+if __name__ == '__main__':
+    total=over_underskud(indtægter,udgifter)
 
     if total <= 0:
         print("I denne måned er du " + str(total) + " i underskud")
     else:
         print("I denne måned har du " + str(total) + " i overskud")
-    return
 
-def størst_udgift(udgifter):
-    højest = udgifter[0]
-    for kategori in udgifter:
-        if udgifter[kategori]>udgifter[højest]:
-            højest = kategori
-        print("Du har brugt flest penge på " + højest)
-
-
-print(over_underskud(indtægter, udgifter), størst_udgift(udgifter))
-
+print(over_underskud(indtægter, udgifter))
+print("Du har brugt flest penge på",størst_udgift(udgifter))
